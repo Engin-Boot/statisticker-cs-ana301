e31a1d863d1e4e62093bfd39ab6f06323ce9f89d
ana301 <31302345+ana301@users.noreply.github.com>
2020-08-31 21:26:04 +0530

    Update StatsUnitTest.cs

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -11,20 +11,21 @@ namespace Statistics.Test
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
-                new List<float>{1.5, 8.9, 3.2, 4.5});
+                new List<float>(){1.5, 8.9, 3.2, 4.5});
             float epsilon = 0.001F;
             Assert.True(Math.Abs(computedStats.ave - 4.525) <= epsilon);
             Assert.True(Math.Abs(computedStats.max - 8.9) <= epsilon);
             Assert.True(Math.Abs(computedStats.min - 1.5) <= epsilon);
         }
+        [Fact]
         public void ReportsNaNForEmptyInput()
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
-                new List<float>{});
-            Assert.True(Double.isNan(computedStats.ave));
-            Assert.True(Double.isNan(computedStats.min));
-            Assert.True(Double.isNan(computedStats.max));
+                new List<float>(){});
+            Assert.True(Double.IsNaN(computedStats.ave));
+            Assert.True(Double.IsNaN(computedStats.min));
+            Assert.True(Double.IsNaN(computedStats.max));
             //All fields of computedStats (average, max, min) must be
             //Double.NaN (not-a-number), as described in
             //https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
