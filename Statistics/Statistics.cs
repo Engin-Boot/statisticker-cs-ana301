8516e11019546e1ba675bf05f633c17be25a749d
ana301 <31302345+ana301@users.noreply.github.com>
2020-08-31 20:22:38 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 
 namespace Statistics
 {
+    class Stats{
+       internal float ave;
+       internal float max;
+       internal float min;
+        
+       Stats(){
+       ave=0.0;
+       max=0.0;
+       min=Single.MaxValue;
+       }
+    }
     public class StatsComputer
     {
         public Stats CalculateStatistics(List<float> numbers) {
             //Implement statistics here
+            Stats ans=new Stats();
+            float sum=0.0;
+            for(int i=0;i<numbers.Length;i++){
+                 
+                sum+=numbers[i];
+                ans.max=Math.max(max,numbers[i]);
+                ans.min=Math.min(min,numbers[i]);
+                
+            }
+            ans.ave=sum/numbers.Length;
+            if(numbers.Length==0)
+                ans.min=0.0;
+            return ans;
+            
         }
     }
 }
